20c5886cb786a7ae150e89b91f67ca8fdd503a8b
avnirastogi14 <avnirastogi363@gmail.com>
2026-04-17 14:41:45 +0530

    fix mongo uri

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-MONGO_URI = "mongodb+srv://admin:<admin123>@cluster0.qim0t4s.mongodb.net/?appName=Cluster0"
+MONGO_URI = "mongodb+srv://admin:admin123@cluster0.qim0t4s.mongodb.net/test?retryWrites=true&w=majority"
 
 DB_NAME = "test"
 
